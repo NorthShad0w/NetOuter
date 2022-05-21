499c435af355f4ca68df486c7dbffde625e4718c
NorthShad0w <ldc992426347@gmail.com>
2022-05-21 06:03:10 -0700

    update

--- pkg/checktcp/checktcp.go
+++ pkg/checktcp/checktcp.go
@@ -3,14 +3,16 @@ package checktcp
 import (
 	"fmt"
 	"net"
+	"time"
 )
 
 func Checktcp(address string) {
-	conn, err := net.Dial("tcp", address)
+	conn, err := net.DialTimeout("tcp", address, 2*time.Second)
 	if err != nil {
 		fmt.Println(address + "blocked")
 		return
 	}
 	conn.Close()
 	fmt.Println(address + " can access")
+	return
 }
