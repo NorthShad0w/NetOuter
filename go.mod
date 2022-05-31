9fcf71bb235b4e628b5a6c23d6a8adbaa2ee92ab
NorthShad0w <ldc992426347@gmail.com>
2022-05-31 10:43:23 -0700

    Remove the go-snmp dependency

--- go.mod
+++ go.mod
@@ -2,13 +2,11 @@ module NetOuter
 
 go 1.18
 
-require (
-	github.com/beevik/ntp v0.3.0
-	github.com/gosnmp/gosnmp v1.34.0
-)
+require github.com/beevik/ntp v0.3.0
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
 	golang.org/x/net v0.0.0-20210428140749-89ef3d95e781 // indirect
 	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
 )
--- pkg/checksnmp/checksnmp.go
+++ pkg/checksnmp/checksnmp.go
@@ -1,22 +1,28 @@
 package checksnmp
 
 import (
+	"bufio"
 	"fmt"
-
-	g "github.com/gosnmp/gosnmp"
+	"net"
+	"time"
 )
 
 func Checksnmp(ip string) {
-	g.Default.Target = ip
-	err := g.Default.Connect()
+	p := make([]byte, 2048)
+	to_sent := []byte{0x30, 0x37, 0x02, 0x01, 0x01, 0x04, 0x06, 0x70, 0x75, 0x62, 0x6c, 0x69, 0x63, 0xa0, 0x2a, 0x02, 0x04, 0x60, 0x32, 0x86, 0x48, 0x02, 0x01, 0x00, 0x02, 0x01, 0x00, 0x30, 0x1c, 0x30, 0x0c, 0x06, 0x08, 0x2b, 0x06, 0x01, 0x02, 0x01, 0x01, 0x04, 0x00, 0x05, 0x00, 0x30, 0x0c, 0x06, 0x08, 0x2b, 0x06, 0x01, 0x02, 0x01, 0x01, 0x07, 0x00, 0x05, 0x00}
+	conn, err := net.Dial("udp", ip+":161")
+	conn.SetDeadline(time.Now().Add(2 * time.Second))
 	if err != nil {
-		fmt.Println("[-] UDP 161 is blocked")
+		fmt.Printf("Some error %v", err)
+		return
 	}
-	defer g.Default.Conn.Close()
-	oids := []string{"1.3.6.1.2.1.1.4.0", "1.3.6.1.2.1.1.7.0"}
-	_, err = g.Default.Get(oids) // Get() accepts up to g.MAX_OIDS
-	if err != nil {
-		fmt.Println("[-] UDP 161 is blocked")
+	fmt.Fprintf(conn, string(to_sent))
+
+	_, err = bufio.NewReader(conn).Read(p)
+	if p[3] != 0 {
+		fmt.Println("[*] UDP 161 can access the internet")
+		return
 	}
-	fmt.Println("[*] UDP 161 can access the internet")
+	fmt.Println("[-] UDP 161 May blocked")
+	conn.Close()
 }
